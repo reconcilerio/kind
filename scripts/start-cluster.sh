db9a7e70b6e92b00ca20cba20c9c364badb90cea
Scott Andrews <scott@andrews.me>
2026-05-09 22:57:11 -0400

    first attempt at trusting the local registry
    Signed-off-by: Scott Andrews <scott@andrews.me>

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -4,33 +4,20 @@ set -euo pipefail
 
 name="${1}"
 image="${2}"
-registry="${3:-localhost}"
+registry="${3}"
 registry_ca="${4}"
 
 work_dir="${RUNNER_TEMP}/scothis/kind/${name}"
-cert_dir="${work_dir}/certs"
-if [ -d  "${cert_dir}" ] ; then
+if [ -d  "${work_dir}" ] ; then
     echo "::error title=duplicate kind cluster::another cluster with the name \"${name}\" appears to be in use"
     exit 1
 fi
-mkdir -p "${cert_dir}"
+mkdir -p "${work_dir}"
 
-cat <<EOF > "${cert_dir}/hosts.toml"
-server = "https://${registry}"
-
-[host."https://${registry}"]
-    capabilities = ["pull"]
-EOF
-if [[ "${registry_ca}" != "" ]] ; then
-    echo "    ca = \"/etc/containerd/certs.d/${registry}/ca.pem\"" >> "${cert_dir}/hosts.toml"
-    cp "${registry_ca}" "${cert_dir}/ca.pem"
-fi
-
-
-# create a cluster with the local registry enabled in containerd
 cat <<EOF > "${work_dir}/kind.yaml"
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
+name: "${name}"
 containerdConfigPatches:
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry]
@@ -38,9 +25,43 @@ containerdConfigPatches:
 nodes:
 - role: control-plane
   image: "${image}"
-  extraMounts:
-  - containerPath: "/etc/containerd/certs.d/${registry}"
-    hostPath: "${cert_dir}"
 EOF
 
-kind create cluster "${name}" --config "${work_dir}/kind.yaml" --wait 5m
+kind create cluster --config "${work_dir}/kind.yaml" --wait 5m
+
+if [[ "${registry}" != "" ]] ; then
+  if [[ "${registry_ca}" != "" ]] ; then
+    cert_dir="${work_dir}/certs"
+    mkdir -p "${cert_dir}"
+
+    cp "${registry_ca}" "${cert_dir}/ca.pem"
+    cat <<EOF > "${cert_dir}/hosts.toml"
+server = "https://${registry}"
+
+[host."https://${registry}"]
+  capabilities = ["pull"]
+  ca = "/etc/containerd/certs.d/${registry}/ca.pem"
+EOF
+
+    # copy cert to each node
+    for node in "$(kind get nodes --name "${name}")" ; do
+      docker cp "${cert_dir}" "${name}-control-plane:/etc/containerd/certs.d/${registry}"
+    done
+  fi
+
+  # Document the local registry
+  # https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+  cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "${registry}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
+fi
+fi
+
--- scripts/stop-cluster.sh
+++ scripts/stop-cluster.sh
@@ -4,4 +4,7 @@ set -euo pipefail
 
 name="${1}"
 
+work_dir="${RUNNER_TEMP}/scothis/kind/${name}"
+
 kind delete cluster "${name}"
+rm -rfv "${work_dir}"
