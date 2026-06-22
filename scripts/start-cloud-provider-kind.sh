937f47463aefa59d2ee3d1ad9467be5929dcabed
Scott Andrews <scott@andrews.me>
2026-06-22 11:57:33 -0400

    Action to install and run cloud-provider-kind (#38)
    ```yaml
    steps:
    - name: Run cluster
      uses: reconcilerio/kind@v1
    - uses: reconcilerio/kind/cloud-provider-kind@v1
    ```
    
    Signed-off-by: Scott Andrews <scott@andrews.me>

---
+++ scripts/start-cloud-provider-kind.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+work_dir="${RUNNER_TEMP}/reconcilerio/kind/cloud-provider-kind"
+if [ -d  "${work_dir}" ] ; then
+    echo "::error title=duplicate cloud-provider-kind::another instance of cloud-provider-kind appears to be running"
+    exit 1
+fi
+mkdir -p "${work_dir}"
+
+echo "Starting cloud-provider-kind"
+cloud-provider-kind > "${work_dir}/cloud-provider-kind.log" 2>&1 &
+PID=$!
+echo "${PID}" > "${work_dir}/cloud-provider-kind.pid"
---
+++ scripts/stop-cloud-provider-kind.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+work_dir="${RUNNER_TEMP}/reconcilerio/kind/cloud-provider-kind"
+
+echo "Stopping cloud-provider-kind"
+PID=$(cat "${work_dir}/cloud-provider-kind.pid")
+kill ${PID} || echo "cloud-provider-kind was not running"
+timeout 30 tail --pid=${PID} -f /dev/null || kill -9 ${PID}
+
+echo "##[group]cloud-provider-kind logs"
+  cat "${work_dir}/cloud-provider-kind.log"
+echo "##[endgroup]"
+
+rm -rfv "${work_dir}"
