38bf3e19ac5376015f50ff2bcb5265db801451de
Scott Andrews <scott@andrews.me>
2026-06-21 22:31:22 -0400

    create work_dir
    Signed-off-by: Scott Andrews <scott@andrews.me>

--- scripts/start-cloud-provider-kind.sh
+++ scripts/start-cloud-provider-kind.sh
@@ -7,6 +7,7 @@ if [ -d  "${work_dir}" ] ; then
     echo "::error title=duplicate cloud-provider-kind::another instance of cloud-provider-kind appears to be running"
     exit 1
 fi
+mkdir -p "${work_dir}"
 
 echo "Starting cloud-provider-kind"
 cloud-provider-kind > "${work_dir}/cloud-provider-kind.log" 2>&1 &
