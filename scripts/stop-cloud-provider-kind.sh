d689df7da891cf6f239e653c8fe50ea531da9d38
Scott Andrews <scott@andrews.me>
2026-06-22 08:38:41 -0400

    wait for lb
    Signed-off-by: Scott Andrews <scott@andrews.me>

--- scripts/stop-cloud-provider-kind.sh
+++ scripts/stop-cloud-provider-kind.sh
@@ -6,7 +6,7 @@ work_dir="${RUNNER_TEMP}/reconcilerio/kind/cloud-provider-kind"
 
 echo "Stopping cloud-provider-kind"
 PID=$(cat "${work_dir}/cloud-provider-kind.pid")
-kill ${PID} || || echo "cloud-provider-kind was not running"
+kill ${PID} || echo "cloud-provider-kind was not running"
 timeout 30 tail --pid=${PID} -f /dev/null || kill -9 ${PID}
 
 echo "##[group]cloud-provider-kind logs"
