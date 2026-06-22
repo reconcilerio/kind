898acaabb36cb5e6ec35ee2fcf1822d013afcf94
Scott Andrews <scott@andrews.me>
2026-06-22 10:10:10 -0400

    use prebuilt cloud-provider-kind when available
    Signed-off-by: Scott Andrews <scott@andrews.me>

--- scripts/start-cloud-provider-kind.sh
+++ scripts/start-cloud-provider-kind.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-cloud_provider_kind="${1}"
-
 work_dir="${RUNNER_TEMP}/reconcilerio/kind/cloud-provider-kind"
 if [ -d  "${work_dir}" ] ; then
     echo "::error title=duplicate cloud-provider-kind::another instance of cloud-provider-kind appears to be running"
@@ -12,6 +10,6 @@ fi
 mkdir -p "${work_dir}"
 
 echo "Starting cloud-provider-kind"
-"${cloud_provider_kind}" > "${work_dir}/cloud-provider-kind.log" 2>&1 &
+cloud-provider-kind > "${work_dir}/cloud-provider-kind.log" 2>&1 &
 PID=$!
 echo "${PID}" > "${work_dir}/cloud-provider-kind.pid"
